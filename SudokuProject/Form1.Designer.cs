5454f6a250f2a24537ce37dce3f8ec176d7bef7b
Vika Stouk <vikastoukv@Gmail.com>
2022-10-30 22:38:51 +0300

    Изменение интерфейса начального экрана приложения

--- SudokuProject/Form1.Designer.cs
+++ SudokuProject/Form1.Designer.cs
@@ -28,74 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.EasyB = new System.Windows.Forms.Button();
             this.MediumB = new System.Windows.Forms.Button();
             this.HardB = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.LeadB = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ExitB = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // EasyB
             // 
-            this.EasyB.Location = new System.Drawing.Point(165, 238);
+            this.EasyB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.EasyB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.EasyB.Location = new System.Drawing.Point(45, 258);
+            this.EasyB.Margin = new System.Windows.Forms.Padding(4);
             this.EasyB.Name = "EasyB";
-            this.EasyB.Size = new System.Drawing.Size(148, 44);
+            this.EasyB.Size = new System.Drawing.Size(167, 41);
             this.EasyB.TabIndex = 0;
-            this.EasyB.Text = "Легкий";
+            this.EasyB.Text = "Easy";
             this.EasyB.UseVisualStyleBackColor = true;
             // 
             // MediumB
             // 
-            this.MediumB.Location = new System.Drawing.Point(165, 288);
+            this.MediumB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.MediumB.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.MediumB.Location = new System.Drawing.Point(45, 307);
+            this.MediumB.Margin = new System.Windows.Forms.Padding(4);
             this.MediumB.Name = "MediumB";
-            this.MediumB.Size = new System.Drawing.Size(148, 44);
+            this.MediumB.Size = new System.Drawing.Size(167, 41);
             this.MediumB.TabIndex = 1;
-            this.MediumB.Text = "Середній";
+            this.MediumB.Text = "Medium";
             this.MediumB.UseVisualStyleBackColor = true;
             // 
             // HardB
             // 
-            this.HardB.Location = new System.Drawing.Point(165, 338);
+            this.HardB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.HardB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.HardB.Location = new System.Drawing.Point(45, 356);
+            this.HardB.Margin = new System.Windows.Forms.Padding(4);
             this.HardB.Name = "HardB";
-            this.HardB.Size = new System.Drawing.Size(148, 44);
+            this.HardB.Size = new System.Drawing.Size(167, 41);
             this.HardB.TabIndex = 2;
-            this.HardB.Text = "Складний";
+            this.HardB.Text = "Hard";
             this.HardB.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(144, 198);
+            this.label1.Location = new System.Drawing.Point(45, 218);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(185, 19);
+            this.label1.Size = new System.Drawing.Size(102, 27);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Оберіть рівень складності:";
+            this.label1.Text = "Difficultly:";
             // 
             // LeadB
             // 
-            this.LeadB.Location = new System.Drawing.Point(165, 430);
+            this.LeadB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.LeadB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LeadB.Location = new System.Drawing.Point(300, 444);
+            this.LeadB.Margin = new System.Windows.Forms.Padding(4);
             this.LeadB.Name = "LeadB";
-            this.LeadB.Size = new System.Drawing.Size(148, 44);
+            this.LeadB.Size = new System.Drawing.Size(167, 41);
             this.LeadB.TabIndex = 4;
-            this.LeadB.Text = "Рейтинг";
+            this.LeadB.Text = "Score";
             this.LeadB.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(150, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(285, 131);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Yi Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(274, 131);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 38);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Stukalenko Victoria CS-19-1\r\n\r\n";
+            // 
+            // ExitB
+            // 
+            this.ExitB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ExitB.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ExitB.Location = new System.Drawing.Point(199, 520);
+            this.ExitB.Margin = new System.Windows.Forms.Padding(4);
+            this.ExitB.Name = "ExitB";
+            this.ExitB.Size = new System.Drawing.Size(167, 41);
+            this.ExitB.TabIndex = 7;
+            this.ExitB.Text = "Exit";
+            this.ExitB.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(126, 451);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(198, 27);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Check your results:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 27F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(478, 517);
+            this.ClientSize = new System.Drawing.Size(565, 574);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ExitB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.LeadB);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.HardB);
             this.Controls.Add(this.MediumB);
             this.Controls.Add(this.EasyB);
-            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Font = new System.Drawing.Font("Microsoft Yi Baiti", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sudoku";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,5 +178,9 @@
         private Button HardB;
         private Label label1;
         private Button LeadB;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Button ExitB;
+        private Label label3;
     }
 }
